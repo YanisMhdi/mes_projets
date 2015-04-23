bb5c57bd8a1d16e1a2f0766dc5b1fa1313feebda
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 20:54:35 +0200

    Update struct.h

--- my_printf/struct.h
+++ my_printf/struct.h
@@ -1,13 +1,3 @@
-/*
-** struct.h for  in /home/andrew/myprintf
-** 
-** Made by LANCIEN Andrew
-** Login   <lancie_a@etna-alternance.net>
-** 
-** Started on  Fri Mar  6 11:45:05 2015 LANCIEN Andrew
-** Last update Fri Mar  6 11:59:06 2015 LANCIEN Andrew
-*/
-
 #ifndef STRUCT_H__
 #define STRUCT_H__
 
