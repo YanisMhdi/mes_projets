3379538643e378693bb31449b40ccaedad2c8e36
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 21:00:28 +0200

    Update my_monster.h

--- Easter_Project/my_monster.h
+++ Easter_Project/my_monster.h
@@ -1,13 +1,3 @@
-/*
-** my_monster.h for  in /home/regency/Midgar/irican_f
-** 
-** Made by IRICANIN Filip
-** Login   <irican_f@etna-alternance.net>
-** 
-** Started on  Fri Oct 10 11:04:20 2014 IRICANIN Filip
-** Last update Fri Oct 10 13:16:08 2014 IRICANIN Filip
-*/
-
 #ifndef __MY_MONSTER_H__
 #define __MY_MONSTER_H__
 
