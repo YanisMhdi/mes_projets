e98da11994638d95935bb628dfd0102f7b60307f
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 21:03:21 +0200

    Update check_ship.c

--- my_battleship/check_ship.c
+++ my_battleship/check_ship.c
@@ -1,22 +1,9 @@
-/*
-** check_ship.c for  in /home/mohamd_y/Piscine/Langage_C/my_battleship
-** 
-** Made by MOHAMDI Yanis
-** Login   <mohamd_y@etna-alternance.net>
-** 
-** Started on  Sat Oct  4 10:07:34 2014 MOHAMDI Yanis
-** Last update Sat Oct  4 14:00:00 2014 MOHAMDI Yanis
-*/
 void	my_putchar(char c);
 void	my_putstr(char *str);
 void	my_put_nbr(int n);
 void    display_map(int battlefield[10][10]);
 
-/*
-** FONCTION CHECH SHIP
-** Permet de gerer si tout les bateaux sont coules
-** Mets fin a la partie
-*/
+
 void	check_ship(int battlefield[10][10], int *end, int count)
 {
   int	i;
