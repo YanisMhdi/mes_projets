0ad43ad44c7e5b8e29b144fa900ecd30814e1af5
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 20:56:33 +0200

    Update DrawMagic.c

--- Easter_Project/DrawMagic.c
+++ Easter_Project/DrawMagic.c
@@ -1,13 +1,3 @@
-/*
-** DrawMagic.c for  in /home/regency
-** 
-** Made by IRICANIN Filip
-** Login   <irican_f@etna-alternance.net>
-** 
-** Started on  Sun Apr 12 12:10:36 2015 IRICANIN Filip
-** Last update Sun Apr 12 19:42:45 2015 IRICANIN Filip
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <SDL/SDL.h>
