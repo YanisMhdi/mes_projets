4b2379660220a7372fa2804056763c274d9f9548
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 21:04:58 +0200

    Update display_count.c

--- my_battleship/display_count.c
+++ my_battleship/display_count.c
@@ -1,20 +1,8 @@
-/*
-** display_count.c for  in /home/mohamd_y/Piscine/Langage_C/my_battleship
-** 
-** Made by MOHAMDI Yanis
-** Login   <mohamd_y@etna-alternance.net>
-** 
-** Started on  Sat Oct  4 09:08:05 2014 MOHAMDI Yanis
-** Last update Sat Oct  4 10:50:25 2014 MOHAMDI Yanis
-*/
 void	my_putchar(char c);
 void	my_putstr(char *str);
 void	my_put_nbr(int n);
 
-/*
-** FONCTION DISPLAY COUNT
-** Affiche le nombre de coups
-*/
+
 void	display_count(int count)
 {
   if (count < 2)
