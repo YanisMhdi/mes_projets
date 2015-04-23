2d77e2ef8eec0ae75210a6693c7831fac8dc93b4
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 21:08:06 +0200

    Update nox_o.php

--- projet_nox/nox_o.php
+++ projet_nox/nox_o.php
@@ -1,12 +1,4 @@
 <?php
-// nox_o.php for  in /home/diman_i/no_x
-// 
-// Made by Diman Ioan-Adrian
-// Login   <diman_i@etna-alternance.net>
-// 
-// Started on  Mon Nov 10 16:07:17 2014 Diman Ioan-Adrian
-// Last update Mon Nov 10 16:51:38 2014 MOHAMDI Yanis
-//
 
 function	nox_o($tab)
 {
