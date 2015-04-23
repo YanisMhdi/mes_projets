485e854d4b8313996c03e3e2640276a841a86af0
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 20:57:46 +0200

    Update Sounds.c

--- Easter_Project/Sounds.c
+++ Easter_Project/Sounds.c
@@ -1,13 +1,3 @@
-/*
-** Sounds.c for  in /home/regency
-** 
-** Made by IRICANIN Filip
-** Login   <irican_f@etna-alternance.net>
-** 
-** Started on  Sun Apr 12 12:40:10 2015 IRICANIN Filip
-** Last update Sun Apr 12 12:40:33 2015 IRICANIN Filip
-*/
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
