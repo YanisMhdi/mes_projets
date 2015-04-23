836e15922754811fdc3658a0898b4b716ff9736d
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 21:04:34 +0200

    Update choice.c

--- my_battleship/choice.c
+++ my_battleship/choice.c
@@ -1,12 +1,3 @@
-/*
-** choice.c for  in /home/mohamd_y/Piscine/Langage_C/my_battleship
-** 
-** Made by MOHAMDI Yanis
-** Login   <mohamd_y@etna-alternance.net>
-** 
-** Started on  Sat Oct  4 11:58:09 2014 MOHAMDI Yanis
-** Last update Sat Oct  4 12:22:30 2014 MOHAMDI Yanis
-*/
 void	my_putstr(char *str);
 int	my_strcmp(char *s1, char *s2);
 char	*readLine();
