995ff582790997bad998f31466058fdbd809fe2f
YanisMhdi <mohamd_y@etna-alternance.net>
2015-04-23 21:05:59 +0200

    Update main.c

--- my_battleship/main.c
+++ my_battleship/main.c
@@ -1,22 +1,10 @@
-/*
-** main.c for  in /home/mohamd_y/Piscine/Langage_C/my_battleship
-** 
-** Made by MOHAMDI Yanis
-** Login   <mohamd_y@etna-alternance.net>
-** 
-** Started on  Fri Oct  3 19:39:55 2014 MOHAMDI Yanis
-** Last update Sat Oct  4 13:54:45 2014 MOHAMDI Yanis
-*/
 void    start(int count, int battlefield[10][10]);
 void    startIA(int count, int battlefield[10][10], int bf_player[10][10]);
 void	my_putstr(char *str);
 char	*readLine();
 int	choice();
 
-/*
-** FONCTION PRINCIPALE
-** Cette fonction initialise et lance une partie de jeu.
-*/
+
 int	main(int argc, char **argv)
 {
   int		count;
